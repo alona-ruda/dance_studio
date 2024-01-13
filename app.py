a04b18b59b56ce3a813ee19b91d3f205270a4cd3
Alona <63323474+alona-ruda@users.noreply.github.com>
2024-01-13 21:19:11 +0000

    Reuse dropdown for navbar

--- app.py
+++ app.py
@@ -32,14 +32,6 @@ def get_dance_class(dance_class_id):
     return dance_class
 
 
-def get_dance_class_by_name(dance_class_name):
-    conn = get_db_connection()
-    dance_class = conn.execute('SELECT * FROM dance_classes WHERE dance_class_name = ?',
-                        (dance_class_name,)).fetchone()
-    conn.close()
-    if dance_class is None:
-        abort(404)
-    return dance_class
 
 def get_dance_classes_for_navbar():
     conn = get_db_connection()
@@ -51,50 +43,35 @@ def get_dance_classes_for_navbar():
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# @app.route('/')
-# def index():
-#     get_dance_classes_for_navbar()
-#     # get_dance_class(dance_class_id)
-#     # conn = get_db_connection()
-#     # dance_classes = conn.execute('SELECT * FROM dance_classes').fetchall()
-#     # conn.close()
-#     return render_template('index.html', dance_classes=dance_classes)
-
 
 @app.route('/')
 def index():
-    # get_dance_classes_for_navbar()
-    # get_dance_class(dance_class_id)
-    conn = get_db_connection()
-    dance_classes = conn.execute('SELECT * FROM dance_classes').fetchall()
-    conn.close()
+    dance_classes = get_dance_classes_for_navbar()
     return render_template('index.html', dance_classes=dance_classes)
-
-
-
-
 
 
 @app.route('/teachers')
 def teachers():
+    dance_classes = get_dance_classes_for_navbar()
     conn = get_db_connection()
     teachers = conn.execute('SELECT * FROM teachers').fetchall()
     conn.close()
-    return render_template('teachers.html', teachers=teachers)
+    return render_template('teachers.html', teachers=teachers, dance_classes=dance_classes)
 
 
 @app.route('/teacher/<int:teacher_id>')
 def teacher(teacher_id):
+    dance_classes = get_dance_classes_for_navbar()
     teacher = get_teacher(teacher_id)
-    return render_template('teacher.html', teacher=teacher)
+    return render_template('teacher.html', teacher=teacher, dance_classes=dance_classes)
 
 
 @app.route('/create_teacher', methods=('GET', 'POST'))
 def create_teacher():
+    dance_classes = get_dance_classes_for_navbar()
     if request.method == 'POST':
         name = request.form['name']
         surname = request.form['surname']
-
         if not name:
             flash('Name is required!')
         elif not surname:
@@ -106,18 +83,16 @@ def create_teacher():
             conn.commit()
             conn.close()
             return redirect(url_for('teachers'))
-
-    return render_template('create_teacher.html')
+    return render_template('create_teacher.html', dance_classes=dance_classes)
 
 
 @app.route('/teacher/<int:teacher_id>/edit', methods=('GET', 'POST'))
 def edit_teacher(teacher_id):
+    dance_classes = get_dance_classes_for_navbar()
     teacher = get_teacher(teacher_id)
-
     if request.method == 'POST':
         name = request.form['name']
         surname = request.form['surname']
-
         if not name:
             flash('Name is required!')
         elif not surname:
@@ -130,31 +105,26 @@ def edit_teacher(teacher_id):
             conn.commit()
             conn.close()
             return redirect(url_for('teachers'))
-
-    return render_template('edit_teacher.html', teacher=teacher)
+    return render_template('edit_teacher.html', teacher=teacher, dance_classes=dance_classes)
 
 
 @app.route('/teacher/<int:teacher_id>/delete', methods=('POST',))
 def delete_teacher(teacher_id):
+    dance_classes = get_dance_classes_for_navbar()
     teacher = get_teacher(teacher_id)
     conn = get_db_connection()
     conn.execute('DELETE FROM teachers WHERE teacher_id = ?', (teacher_id,))
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(teacher['name']))
-    return redirect(url_for('teachers'))
+    return redirect(url_for('teachers'), dance_classes=dance_classes)
 
-
-# @app.route('/class/<string:dance_class_name>')
-# def dance_class(dance_class_name):
-#     dance_class = get_dance_class(dance_class_name)
-#     return render_template('class.html', dance_class=dance_class)
 
 @app.route('/class/<string:dance_class_id>')
 def dance_class(dance_class_id):
+    dance_classes = get_dance_classes_for_navbar()
     dance_class = get_dance_class(dance_class_id)
-    return render_template('class.html', dance_class=dance_class)
-
+    return render_template('class.html', dance_class=dance_class, dance_classes=dance_classes)
 
 
 if __name__ == '__main__':

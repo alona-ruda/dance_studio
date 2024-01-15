78af9b7b6c14630aa1d4f0dc77270e770cbba24b
Ruda <ruda.lisa.ua@gmail.com>
2024-01-15 16:47:30 +0200

    Formatting code

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
-# from flask_login import LoginManager
-# login_manager = LoginManager()
-# login_manager.init_app(app)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -21,6 +18,21 @@ def get_teacher(teacher_id):
         abort(404)
     return teacher
 
+
+def get_teacher_detail(teacher_id):
+    conn = get_db_connection()
+    teacher = conn.execute('''
+        SELECT teachers.teacher_id, teachers.name, teachers.surname, GROUP_CONCAT(dance_classes.dance_class_name) as class_names
+        FROM teachers 
+        LEFT JOIN teacher_classes ON teachers.teacher_id = teacher_classes.teacher_id
+        LEFT JOIN dance_classes ON teacher_classes.class_id = dance_classes.dance_class_id
+        WHERE teachers.teacher_id = ?
+        GROUP BY teachers.teacher_id
+    ''', (teacher_id,)).fetchall()
+    conn.close()
+    if teacher is None:
+        abort(404)
+    return teacher
 
 
 def get_dance_class(dance_class_id):
@@ -41,14 +53,6 @@ def get_dance_classes_for_navbar():
     return dance_classes
 
 
-def get_classes_of_teacher(teacher_id):
-    conn = get_db_connection()
-    classes_of_teacher = conn.execute('SELECT * FROM teacher_classes WHERE teacher_id = ?',
-                        (teacher_id,)).fetchall()
-    conn.close()
-    return classes_of_teacher
-
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
@@ -60,22 +64,20 @@ def index():
     return render_template('index.html', dance_classes=dance_classes)
 
 
-# @app.route('/teachers')
-# def teachers():
-#     dance_classes = get_dance_classes_for_navbar()
-#     conn = get_db_connection()
-#     teachers = conn.execute('SELECT * FROM teachers').fetchall()
-#     conn.close()
-#     return render_template('teachers.html', teachers=teachers, dance_classes=dance_classes)
+@app.route('/teachers')
+def teachers():
+    dance_classes = get_dance_classes_for_navbar()
+    conn = get_db_connection()
+    teachers = conn.execute('SELECT * FROM teachers').fetchall()
+    conn.close()
+    return render_template('teachers.html', teachers=teachers, dance_classes=dance_classes)
 
 
 @app.route('/teacher/<int:teacher_id>')
 def teacher(teacher_id):
     dance_classes = get_dance_classes_for_navbar()
-    teacher = get_teacher(teacher_id)
-    classes_of_teacher = get_classes_of_teacher(teacher_id)
-    return render_template('teacher.html', teacher=teacher, dance_classes=dance_classes, classes_of_teacher=classes_of_teacher)
-
+    teacher = get_teacher_detail(teacher_id)
+    return render_template('teacher.html', teacher=teacher, dance_classes=dance_classes)
 
 
 @app.route('/create_teacher', methods=('GET', 'POST'))
@@ -132,26 +134,14 @@ def delete_teacher(teacher_id):
     return redirect(url_for('teachers'), dance_classes=dance_classes)
 
 
-@app.route('/class/<string:dance_class_id>')
+@app.route('/class/<int:dance_class_id>')
 def dance_class(dance_class_id):
     dance_classes = get_dance_classes_for_navbar()
     dance_class = get_dance_class(dance_class_id)
     return render_template('class.html', dance_class=dance_class, dance_classes=dance_classes)
 
 
-@app.route('/teachers')
-def teachers():
-    conn = get_db_connection()
-    cursor = conn.execute('''
-        SELECT teachers.teacher_id, teachers.name, teachers.surname, GROUP_CONCAT(dance_classes.dance_class_name) as class_names
-        FROM teachers
-        JOIN teacher_classes ON teachers.teacher_id = teacher_classes.teacher_id
-        JOIN dance_classes ON teacher_classes.class_id = dance_classes.dance_class_id
-        GROUP BY teachers.teacher_id
-    ''')
-    teacher_class_data = cursor.fetchall()
-    conn.close()
-    return render_template('teachers.html', teacher_class_data=teacher_class_data)
+
 
 
 
